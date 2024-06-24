45a93bdb97a7f362b42ffdfde9b4d165f625451e
haruna ishii <eijen.6@gmail.com>
2024-06-24 09:18:23 +0900

    フォームを追加しデザイン修正

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -19,18 +19,30 @@
 <div class="content__inner">
     <h2 class="content__heading">さんお疲れ様です！</h2>
     <div class="attendance__unit">
-        <div class="attendance__item">
-            <h2 class="attendance__item--active">勤務開始</h2>
-        </div>
-        <div class="attendance__item">
-            <h2 class="attendance__item--active">勤務終了</h2>
-        </div>
-        <div class="attendance__item">
-            <h2 class="attendance__item--active">休憩開始</h2>
-        </div>
-        <div class="attendance__item">
-            <h2 class="attendance__item--active">休憩終了</h2>
-        </div>
+        <form action="/clockin" class="attendance__item">
+            @csrf
+            <button class="attendance__submit">
+                <h2 class="attendance__item--active">勤務開始</h2>
+            </button>
+        </form>
+        <form action="/clockOut" class="attendance__item">
+            @csrf
+            <button class="attendance__submit">
+                <h2 class="attendance__item--active">勤務終了</h2>
+            </button>
+        </form>
+        <form action="/" class="attendance__item">
+            @csrf
+            <button class="attendance__submit">
+                <h2 class="attendance__item--active">休憩開始</h2>
+            </button>
+        </form>
+        <form action="/" class="attendance__item">
+            @csrf
+            <button class="attendance__submit">
+                <h2 class="attendance__item--active">休憩終了</h2>
+            </button>
+        </form>
     </div>
 </div>
 @endsection
