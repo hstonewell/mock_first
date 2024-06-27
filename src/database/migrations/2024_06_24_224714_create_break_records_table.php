3f8aeb3c0b230e5930996f54256b72d25aa578ca
haruna ishii <eijen.6@gmail.com>
2024-06-27 22:35:04 +0900

    テーブル修正

---
+++ src/database/migrations/2024_06_24_224714_create_break_records_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBreakRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('break_records', function (Blueprint $table) {
+            $table->id();
+            $table->foreignID('attendance_id')->constrained();
+            $table->timestamp('break_start');
+            $table->timestamp('break_end');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('break_records');
+    }
+}
