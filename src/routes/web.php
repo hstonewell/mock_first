ecc1da3a1626c3039eccfd2b13150394f158cc54
haruna ishii <eijen.6@gmail.com>
2024-06-29 23:42:08 +0900

    出退勤・休憩時のルーティング追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AttendanceController;
+use Illuminate\Auth\Events\Attempting;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
+Route::middleware('auth')->group(function () {
+    Route::get('/index', [AttendanceController::class, 'index']);
+    Route::post('/clockin', [AttendanceController::class, 'clockIn']);
+    Route::post('/clockout', [AttendanceController::class, 'clockOut']);
+    Route::post('/breakstart', [AttendanceController::class, 'breakStart']);
+    Route::post('/breakend', [AttendanceController::class, 'breakEnd']);
+    //Route::post('/export', [ContactController::class, 'export']);
 });
 
-Route::post('/clockin', [AttendanceController::class, 'clockIn']);
-Route::post('/clockout', [AttendanceController::class, 'clockOut']);
