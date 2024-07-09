d3ee12c6bca2232c124c03735cdd92580b2e93ff
haruna ishii <eijen.6@gmail.com>
2024-07-09 23:56:00 +0900

    シーディングファイル追加

---
+++ src/database/seeders/BreakRecordsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\BreakRecord;
+use App\Models\Attendance;
+
+class BreakRecordsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $attendances = Attendance::factory()->count(150)->create();
+
+        $attendances->each(function ($attendance) {
+            BreakRecord::factory()->count(rand(0, 3))->create([
+                'attendance_id' => $attendance->id,
+            ]);
+        });
+    }
+}
