aff2c0ae5bfe18dc0c24549a76fb7d340b764030
haruna ishii <eijen.6@gmail.com>
2024-07-27 22:06:53 +0900

    ヘッダー機能をapp.bladeへ移動

--- src/resources/views/attendance.blade.php
+++ src/resources/views/attendance.blade.php
@@ -4,17 +4,6 @@
 <link rel="stylesheet" href="{{ asset('css/attendance.css')}}">
 @endsection
 
-@section('link')
-<div class="header__link">
-    <a href="/index" class="header__link--item">ホーム</a>
-    <a href="/attendance" class="header__link--item" disabled>日付一覧</a>
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-        <button type="button" class="header__link--item">ログアウト</button>
-    </form>
-</div>
-@endsection
-
 @section('content')
 <div class="attendance-list">
     <form method="POST" action="{{ route('attendance.date') }}" class="attendance-date">
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -4,17 +4,6 @@
 <link rel="stylesheet" href="{{ asset('css/index.css')}}">
 @endsection
 
-@section('link')
-<div class="header__link">
-    <a href="/index" class="header__link--item">ホーム</a>
-    <a href="/attendance" class="header__link--item">日付一覧</a>
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-        <button type="header__link--item" class="header__link--item">ログアウト</button>
-    </form>
-</div>
-@endsection
-
 @section('content')
 <div class="content__inner">
     @if(session('message'))
@@ -29,28 +18,28 @@
     <div class="attendance__unit">
         <form action="/clockin" method="POST" class="attendance__item">
             @csrf
-            <button class="attendance__submit" {{ isset($attendance) ? 'disabled' : '' }}>
+            <button class="attendance__submit" {{ $clockInDisabled ? 'disabled' : '' }}>
                 <h2 class="attendance__item--active">勤務開始</h2>
             </button>
         </form>
 
         <form action="/clockout" method="POST" class="attendance__item">
             @csrf
-            <button class="attendance__submit" {{ isset($attendance) && is_null($attendance->clock_out) ? '' : 'disabled' }}>
+            <button class="attendance__submit" {{ $clockOutDisabled ? 'disabled' : '' }}>
                 <h2 class="attendance__item--active">勤務終了</h2>
             </button>
         </form>
 
         <form action="/breakstart" method="POST" class="attendance__item">
             @csrf
-            <button class="attendance__submit" {{ isset($attendance) && is_null($attendance->clock_out) && (!isset($latestBreak) || !is_null($latestBreak->break_end)) ? '' : 'disabled' }}>
+            <button class="attendance__submit" {{ $breakStartDisabled ? 'disabled' : '' }}>
                 <h2 class="attendance__item--active">休憩開始</h2>
             </button>
         </form>
 
         <form action="/breakend" method="POST" class="attendance__item">
             @csrf
-            <button class="attendance__submit" {{ isset($latestBreak) && is_null($latestBreak->break_end) ? '' : 'disabled' }}>
+            <button class="attendance__submit" {{ $breakEndDisabled ? 'disabled' : '' }}>
                 <h2 class="attendance__item--active">休憩終了</h2>
             </button>
         </form>
