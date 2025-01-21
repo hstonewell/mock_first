a724dbebc90e85a70f7f4d9e3182b691271277fb
haruna ishii <eijen.6@gmail.com>
2025-01-21 10:32:33 +0200

    Add custom email notifications

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Notifications\UserCustomVerifyEmail;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -47,4 +48,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new UserCustomVerifyEmail);
+    }
 }
